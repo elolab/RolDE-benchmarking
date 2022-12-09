53a2d3cff0a1a40cd302cafcdb5b50b90c1ea1e9
Tommi Välikangas <tsvali@utu.fi>
2022-12-09 09:42:49 +0200

    Update Scripts

--- getpAUC.R
+++ getpAUC.R
@@ -1,5 +1,5 @@
 library(pROC)
-getAUC<-function(results, reference.prot, spike.prot){
+getAUC<-function(results, reference.prot, spike.prot, method){
   
   #Reference proteins are all the proteins in the data.
   
@@ -21,21 +21,22 @@ getAUC<-function(results, reference.prot, spike.prot){
   values=c(as.numeric(as.character(results[,2])))
   locs=length(which(is.na(values)))
   
-  range.vals=as.numeric(range(results[,2], na.rm = T))
-  
   #For a fair comparison of the methods, if we have NA values in the result list, we want to replace them with some large value so that features with NA result statistics get put at the bottom of the result list.
   
-  if(length(locs)>0){ 
-    if(range.vals[2]>1){ #Our statistic is not p-values
+  if(length(locs)>0){
+    if(method=="BETR" | method=="Timecourse" | method=="Limma" | method=="OmicsLonDA"){ #Our statistic is not p-values
+      max.val=max(na.omit(values))
+      imp.vals=sample(x = seq(from=max.val, to=0, by=0.0000001), size = locs, replace=T) #just some artificial large values placing the features randomly at the bottom of the result list, since we have no DE information regarding them.
+      values[is.na(values)] <- imp.vals
+    }else if(method=="RolDE"){ #just in case, RolDE should not produce any missing values
       max.val=max(na.omit(values))
       imp.vals=sample(x = seq(from=max.val, to=max.val+(locs*1000), by=1), size = locs, replace=T) #just some artificial large values placing the features randomly at the bottom of the result list, since we have no DE information regarding them.
       values[is.na(values)] <- imp.vals
-    }else{ #we have p-values as our statistic.
+    }else{ #we have p-values as our statistic
       max.val=max(na.omit(values))
       imp.vals=sample(x = seq(from=max.val, to=1, by=0.0000001), size = locs, replace=T) #just some artificial large p-value placing the features randomly at the bottom of the result list, since we have no DE information regarding them.
       values[is.na(values)]=imp.vals
     }
-    
   }
   roc=roc(classes, values, direction = ">")
   auc.val=round(auc(roc,partial.auc=c(1.0,0.9), partial.auc.correct=T,allow.invalid.partial.auc.correct=T)[1],digits=5)# #set for partial auc
